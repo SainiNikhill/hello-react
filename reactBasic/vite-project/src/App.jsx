cb4b602547fbefe77d31de9abea89b50de21593e
nikhil <hp5cd1394q34@outlook.com>
2025-02-26 05:59:12 +0530

    Basics

---
+++ reactBasic/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const App = ()=>
+{
+  return(
+    <div>
+      <h2 className='text-5xl bg-amber-500 text-black px-10 py-2' >Hello guys </h2>
+      <h1>Hi everyone! how is all going</h1>
+    </div>
+  )
+}
+export default App
